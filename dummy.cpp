be958e3aa74caefa2405192f27c3ce94792c476d
pitu <pitu.danilo@hotmail.com>
2016-04-30 17:46:07 -0300

    tuto git

---
+++ dummy.cpp
@@ -0,0 +1,19 @@
+/*
+==git tuto==
+initialize a repo:
+git init
+
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int i = 0;
+    while (i < 10)
+
+		cout << i;
+    return 0;
+}
